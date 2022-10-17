784a03bdfdd15b0c5a8589f277415f62e48fb9d9
ENGRSMYLE <101359280+ENGRSMYLE@users.noreply.github.com>
2022-10-17 01:58:03 +0100

    Day one learning C

---
+++ dayone.c
@@ -0,0 +1,11 @@
+// Day one
+#include <stdio.h> // standard input and output library
+
+int main(void)
+{
+    int myAge = 17;
+    printf("Hello , world");
+    printf("I am %d years old", myAge); // %d is a format specifier
+    return 0;                           // the program will return 0 when there is no bug
+                                        // if there is bug it will return 1
+}
